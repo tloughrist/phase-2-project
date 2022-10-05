12fef746039ff6a3dd2f3355e75d70dd0ed49626
timloughrist <tim.loughrist@gmail.com>
2022-10-05 16:40:39 -0500

    styling complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home({ isLoaded }) {
         <div className="display-container">
             <div>
                 <h1>Welcome to inFormation!</h1>
-                <h2>This is an app to help people stay in contact with one another.</h2>
-                <h2>By creating or joining formations, you'll be able to keep track of whatever information your contacts want to share with you.</h2> <h2>Likewise, you have control over what information you share with others.</h2>
+                <h3>This is an app to help people stay in contact with one another.</h3>
+                <h3>By creating or joining formations, you'll be able to keep track of whatever information your contacts want to share with you.</h3> <h3>Likewise, you have control over what information you share with others.</h3>
             </div>
         </div>
     );
