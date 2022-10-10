c0e732557a83636734dd2660c601407eeb1b62a4
timloughrist <tim.loughrist@gmail.com>
2022-10-10 17:22:17 -0500

    fix controlled personal info form

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
 function PersonalInfo({ currentUser, token, patchCurrentUser }) {
 
+    const [name, setName] = useState();
+    const [dob, setDob] = useState();
+    const [pic, setPic] = useState();
+    const [pronouns, setPronouns] = useState();
+    const [phone, setPhone] = useState();
+    const [email, setEmail] = useState();
+    const [address, setAddress] = useState();
+    const [notes, setNotes] = useState();
+
     function handleMyInfoSubmit(e) {
         e.preventDefault();
         const newUserObj = {
-            name: e.target.name.value,
-            dob: e.target.dob.value,
-            pic: e.target.picurl.value,
-            pronouns: e.target.pronouns.value,
-            phone: e.target.phoneno.value,
-            email: e.target.email.value,
-            address: e.target.address.value,
-            notes: e.target.addnotes.value
+            name: name,
+            dob: dob,
+            pic: pic,
+            pronouns: pronouns,
+            phone: phone,
+            email: email,
+            address: address,
+            notes: notes
         };
         alert("Information updated");
         return patchCurrentUser(newUserObj);
@@ -38,21 +47,21 @@ function PersonalInfo({ currentUser, token, patchCurrentUser }) {
                 <form className="form-card" id="myuserform" onSubmit={handleMyInfoSubmit} >
                     <h2 className="form-element">Change Information</h2>
                     <label className="form-label" htmlFor="name">Name</label>
-                    <input className="form-element" type="text" name="name" defaultValue={currentUser.name} />
+                    <input className="form-element" type="text" name="name" defaultValue={currentUser.name} onChange={e => setName(e.target.value)}/>
                     <label className="form-label" htmlFor="dob">Birthday</label>
-                    <input className="form-element" type="date" name="dob" defaultValue={currentUser.dob} />
+                    <input className="form-element" type="date" name="dob" defaultValue={currentUser.dob} onChange={e => setDob(e.target.value)}/>
                     <label className="form-label" htmlFor="pronouns">Pronouns</label>
-                    <input className="form-element" type="text" name="pronouns" defaultValue={currentUser.pronouns} />
-                    <label className="form-label" htmlFor="phone1no">Phone Number</label>
-                    <input className="form-element" type="tel" name="phoneno" defaultValue={currentUser.phone} />
+                    <input className="form-element" type="text" name="pronouns" defaultValue={currentUser.pronouns} onChange={e => setPronouns(e.target.value)}/>
+                    <label className="form-label" htmlFor="phoneno">Phone Number</label>
+                    <input className="form-element" type="tel" name="phoneno" defaultValue={currentUser.phone} onChange={e => setPhone(e.target.value)}/>
                     <label className="form-label" htmlFor="email">Email Address</label>
-                    <input className="form-element" type="email" name="email" defaultValue={currentUser.email} />
+                    <input className="form-element" type="email" name="email" defaultValue={currentUser.email} onChange={e => setEmail(e.target.value)}/>
                     <label className="form-label" htmlFor="address">Address</label>
-                    <input className="form-element" type="textbox" name="address" defaultValue={currentUser.address} />
+                    <input className="form-element" type="textbox" name="address" defaultValue={currentUser.address} onChange={e => setAddress(e.target.value)}/>
                     <label className="form-label" htmlFor="picurl">Picture Url</label>
-                    <input className="form-element" type="url" name="picurl" defaultValue={currentUser.pic} />
+                    <input className="form-element" type="url" name="picurl" defaultValue={currentUser.pic} onChange={e => setPic(e.target.value)}/>
                     <label className="form-label" htmlFor="addnotes">Additional Notes</label>
-                    <input className="form-element" type="textbox" name="addnotes" defaultValue={currentUser.notes} />
+                    <input className="form-element" type="textbox" name="addnotes" defaultValue={currentUser.notes} onChange={e => setNotes(e.target.value)}/>
                     <input className="form-element" type="submit"></input>
                 </form>
             </div>
